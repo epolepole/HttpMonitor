422990fb880446af7a5c421c507c487c4452dcab
epolepole <epolepole@gmail.com>
2018-10-06 21:44:22 +0200

    make formatter empty the queue per iteration

--- jobs/log_formatter_job.py
+++ jobs/log_formatter_job.py
@@ -6,10 +6,11 @@ from jobs.abstract_job import AbstractJob
 
 
 class LogFormatterJob(AbstractJob):
-    def __init__(self, str_log_input_queue: Queue, bom_log_output_queue: list, interval=0.01):
+    def __init__(self, str_log_input_queue: Queue, bom_log_output_queue: list, interval=0.1):
         super().__init__(interval)
         self.__input_queue = str_log_input_queue
         self.__output_queue = bom_log_output_queue
 
     def _iteration(self):
-        heapq.heappush(self.__output_queue, LogParser(self.__input_queue.get()).parse())
+        while not self.__input_queue.empty():
+            heapq.heappush(self.__output_queue, LogParser(self.__input_queue.get()).parse())
