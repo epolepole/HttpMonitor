acd3d07473c9948cf8978c6180e998bb08dd75ec
epolepole <epolepole@gmail.com>
2018-10-07 12:30:12 +0200

    Renaming

--- test/jobs/file_reader_job_test.py
+++ test/jobs/file_reader_job_test.py
@@ -9,29 +9,29 @@ from jobs.file_reader_job import FileReaderJob
 logger = logging.getLogger(__name__)
 
 
-def test_watcher_triggers_callback():
+def test_file_reader_fills_queue():
     output_queue = Queue()
     log_file_path = "./tmp.log"
 
     open(log_file_path, 'a').write("Testing line\n")
-    a_log_watcher = FileReaderJob(log_file_path, output_queue)
-    a_log_watcher.setup()
+    a_file_reader_job = FileReaderJob(log_file_path, output_queue)
+    a_file_reader_job.setup()
 
     open(log_file_path, 'a').write("log line 1\n")
-    a_log_watcher.loop(blocking=False)
+    a_file_reader_job.loop(blocking=False)
     assert output_queue.get() == "log line 1"
 
     open(log_file_path, 'a').write("log line 2\n")
-    a_log_watcher.loop(blocking=False)
+    a_file_reader_job.loop(blocking=False)
     assert output_queue.get() == "log line 2"
 
     open(log_file_path, 'a').write("log line 3\n")
     open(log_file_path, 'a').write("log line 4\n")
-    a_log_watcher.loop(blocking=False)
+    a_file_reader_job.loop(blocking=False)
     assert output_queue.get() == "log line 3"
     assert output_queue.get() == "log line 4"
 
-    a_log_watcher.tear_down()
+    a_file_reader_job.tear_down()
 
     logger.debug("Removing file")
     if os.path.exists(log_file_path):
