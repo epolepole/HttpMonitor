b05b4b5c27706b2cfa53efd3f2f72c71fc9015f2
epolepole <epolepole@gmail.com>
2018-10-07 12:08:29 +0200

    Make abstract job a thread to be able to run it from main class

--- jobs/abstract_job.py
+++ jobs/abstract_job.py
@@ -1,11 +1,16 @@
+import logging
+import threading
 import time
 from abc import ABCMeta, abstractmethod
 
+logger = logging.getLogger(__name__)
 
-class AbstractJob:
+
+class AbstractJob(threading.Thread):
     __metaclass__ = ABCMeta
 
     def __init__(self, interval=0.5):
+        super().__init__()
         self.__interval = interval
         self.__running = False
 
@@ -19,6 +24,11 @@ class AbstractJob:
     def _iteration(self):
         raise NotImplemented("This method should be overridden")
 
+    def run(self):
+        logger.debug("Starting {} job".format(type(self).__name__))
+        self.setup()
+        self.loop()
+
     def loop(self, blocking=True):
         self.__running = True
         while self.__running:
@@ -28,4 +38,7 @@ class AbstractJob:
             time.sleep(self.__interval)
 
     def stop(self):
+        logger.debug("Stopping {} job".format(type(self).__name__))
         self.__running = False
+        time.sleep(self.__interval + 0.1)
+        self.tear_down()
